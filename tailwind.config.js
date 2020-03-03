e2208d6276e161c1fae5803af89ffb8349a2f58c
Andrea DeMers <andrea@andreademers.com>
2020-03-03 14:45:16 -0500

    Add Inter font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 // tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     theme: {
         container: {
             center: true,
         },
-        extend: {}
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        }
     },
     variants: {},
     plugins: [
