9cc0cfa9a3d09ee956bcd8ddde7cb49e1a07187a
Andrea DeMers <andrea@andreademers.com>
2020-03-06 17:11:08 -0500

    Add Alpine.js & comment out Vue.js components

--- src/js/app.js
+++ src/js/app.js
@@ -1,64 +1,37 @@
-// Vue.js
-import Vue from 'vue'
+// Alpine.js
+import 'alpinejs';
 
-Vue.config.productionTip = false;
+// Lazysizes
+import 'lazysizes';
+
+// Uncomment below to use Vue.js components
+// Vue.js
+// import Vue from 'vue'
+
+// Vue.config.productionTip = false;
 
 // Test component {
-Vue.component('test-component', {
-    data() {
-        return {
-            greeting: "Hello World fdkf dkajf!"
-        }
-    },
-});
+// Vue.component('test-component', {
+//     data() {
+//         return {
+//             greeting: "Hello World!"
+//         }
+//     },
+// });
 
 // Horizontal nav
-Vue.component('mobile-toggle', {
-    data() {
-        return {
-            isOpen: false,
-            msg: "Vue.js Hello Andrea!",
-            greeting: "Vue.js greeting!"
-        }
-    },
-});
+// Vue.component('navbar', {
+//     data() {
+//         return {
+//             isOpen: false,
+//             msg: "Vue.js Hello Andrea!",
+//             greeting: "Vue.js greeting!"
+//         }
+//     },
+// });
 
-// Tailwind UI navbar
-Vue.component('tw-ui-navbar', {
-    data() {
-        return {
-            isOpen: false,
-        }
-    },
-    methods: {
-        open() {
-          this.isOpen = true
-          this.$nextTick(() => {
-            this.$refs.closeButton.focus()
-          })
-        },
-        close() {
-          this.isOpen = false
-          this.$nextTick(() => {
-            this.$refs.openButton.focus()
-          })
-        }
-      },
-      watch: {
-        isOpen: {
-          immediate: true,
-          handler(isOpen) {
-            if (isOpen) {
-              document.body.style.setProperty('overflow', 'hidden')
-            } else {
-              document.body.style.removeProperty('overflow')
-            }
-          }
-        }
-      }
-});
+// new Vue({
+//     el: '#app',
+//     delimiters: ['${', '}'],
+// });
 
-new Vue({
-    el: '#app',
-    delimiters: ['${', '}'],
-});
