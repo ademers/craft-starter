ae93bd4d0e5e1881eadd380ae59162fd3bc6c996
Andrea DeMers <andrea@andreademers.com>
2020-03-30 15:24:45 -0400

    Add Minify plugin config file

--- config/minify.php
+++ config/minify.php
@@ -1,1 +1,30 @@
 <?php
+/**
+ * Minify plugin for Craft CMS 3.x
+ *
+ * @link      https://nystudio107.com/
+ * @copyright Copyright (c) 2017 nystudio107
+ * @license   MIT License https://opensource.org/licenses/MIT
+ */
+
+/**
+ * Minify config.php
+ *
+ * This file exists only as a template for the Minify settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'minify.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [
+    // if set to `true` then Minify will not minify anything
+    "disableTemplateMinifying" => false,
+
+    // if set to `true` then Minify will not minify anything if `devMode` is enabled
+    "disableDevModeMinifying" => true,
+];
