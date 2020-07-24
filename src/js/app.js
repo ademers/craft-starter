2bce8f140e1336df0e936fc523797c61cc9b9760
Andrea DeMers <andrea@andreademers.com>
2020-07-24 12:16:48 -0400

    Uninstall & remove Vue JS
    Using Alpine JS instead

--- src/js/app.js
+++ src/js/app.js
@@ -4,34 +4,4 @@ import 'alpinejs';
 // Lazysizes
 import 'lazysizes';
 
-// Uncomment below to use Vue.js components
-// Vue.js
-// import Vue from 'vue'
-
-// Vue.config.productionTip = false;
-
-// Test component {
-// Vue.component('test-component', {
-//     data() {
-//         return {
-//             greeting: "Hello World!"
-//         }
-//     },
-// });
-
-// Horizontal nav
-// Vue.component('navbar', {
-//     data() {
-//         return {
-//             isOpen: false,
-//             msg: "Vue.js Hello Andrea!",
-//             greeting: "Vue.js greeting!"
-//         }
-//     },
-// });
-
-// new Vue({
-//     el: '#app',
-//     delimiters: ['${', '}'],
-// });
 
