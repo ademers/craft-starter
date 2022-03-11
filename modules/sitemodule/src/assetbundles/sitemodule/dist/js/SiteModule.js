fd3b5f88fdaee46368cf33128e1fc862bd78959e
Andrea DeMers <andrea@andreademers.com>
2022-03-11 18:40:41 -0500

    Add custom module

---
+++ modules/sitemodule/src/assetbundles/sitemodule/dist/js/SiteModule.js
@@ -0,0 +1,11 @@
+/**
+ * Site module for Craft CMS
+ *
+ * Site JS
+ *
+ * @author    Andrea DeMers
+ * @copyright Copyright (c) 2022 Andrea DeMers
+ * @link      https://andreademers.com
+ * @package   SiteModule
+ * @since     1.0.0
+ */
